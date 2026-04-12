1d2ace3f590922a2add8c00f5003e88093ef1e40
RaSlabsa <monctrikua@gmail.com>
2026-04-12 12:47:50 +0300

    extended Review model

--- src/Shop.Models/Models/Review.cs
+++ src/Shop.Models/Models/Review.cs
@@ -1,6 +1,47 @@
 public class Review {
     public int Id {get; private set;}
-    public int ProductId {get; private set;}
+    public int ProductId {get; private set;} // Додати зовнішній ключ
+    public int UserId {get; private set;} // Додати зовнішній ключ
     public double Rating {get; private set;} // 1 - 5
     public string Comment {get; private set;} = string.Empty;
+
+    public Review(int productId, int userId, int rating, string comment)
+    {
+        SetProduct(productId);
+        SetUser(userId);
+        SetRating(rating);
+        SetComment(comment);
+    }
+
+    public void SetProduct(int productId)
+    {
+        ProductId = productId;
+        // В майбутньому перевірка чи існує поверх зовнішнього ключа
+    }
+
+    public void SetUser(int userId)
+    {
+        UserId = userId;
+        // В майбутньому перевірка чи існує поверх зовнішнього ключа
+    }
+
+    public void SetRating(int rating)
+    {
+        if (rating > 5 || rating < 0)
+        {
+            throw new Exception("Rating must be in range 0 - 5");
+        }
+
+        Rating = rating;
+    }
+
+    public void SetComment(string comment)
+    {
+        if (string.IsNullOrWhiteSpace(comment))
+        {
+            Comment = string.Empty;
+        }
+
+        Comment = comment.Trim();
+    }
 }
